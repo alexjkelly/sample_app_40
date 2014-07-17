a8535b609516337cf54cc74e20383bbddb2f829f
Alex Kelly <alex.kelly@william-lee.com>
2014-07-17 17:26:44 +0100

    Save progress

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -12,3 +12,15 @@ def valid_signup
 	fill_in "Password",			with: "foobar"
 	fill_in "Confirmation",	with: "foobar"
 end
+
+def sign_in(user, options={})
+	if options[:no_capybara]
+		# Sign in when not using Capybara.
+		remember_token = User.new_remember_token
+		cookies[:remember_token] = remember_token
+		user.update_attribute(:remember_token, User.digest(remember_token))
+	else
+		visit signin_path
+		valid_signin(user)
+	end
+end
